12ff7da0a9ba1d9a10a6b614abefad554d446d4b
Tim Yuen <timothy.yuen22@gmail.com>
2022-01-17 15:00:02 -0500

    add shaders, better mesh creation

---
+++ shaders/mask.shader
@@ -0,0 +1,20 @@
+// https://github.com/renpy/renpy/blob/master/renpy/gl2/live2d.py#L88
+
+shader_type spatial;
+render_mode blend_mix, cull_disabled, depth_draw_alpha_prepass;
+
+uniform sampler2D u_tex_0;
+uniform sampler2D u_tex_1;
+
+varying vec2 v_mask_coord;
+
+void vertex() {
+	v_mask_coord = vec2(VERTEX.x / 2.0 + 0.5, VERTEX.y / 2.0 + 0.5);
+}
+
+void fragment() {
+	vec4 color = texture(u_tex_0, UV);
+	vec4 mask = texture(u_tex_1, v_mask_coord);
+	ALBEDO = color.rgb * mask.a;
+	ALPHA = mask.a;
+}
--- test_screen.gd
+++ test_screen.gd
@@ -1,7 +1,7 @@
 extends Spatial
 
 const TMP_DIR: String = "res://tmp"
-const RES_PATH: String = "res://samples/Haru/"
+const RES_PATH: String = "res://samples/Natori/"
 
 const CUBISM_LOADER_FACTORY_PATH: String = "res://cubism_model_factory.gdns"
 
@@ -9,7 +9,9 @@ onready var root: Spatial = $Root
 
 var model
 var drawables: Array
-var meshes: Array = []
+var meshes: Array = [] # MeshInstances
+var mask_indices: Array = [] # int
+var mesh_materials: Array = [] # Materials for MeshInstances
 var textures: Array = []
 var moc
 
@@ -23,9 +25,9 @@ var opacities = []
 
 func _ready() -> void:
 	var factory = load(CUBISM_LOADER_FACTORY_PATH).new()
-	model = factory.cubism_model(ProjectSettings.globalize_path(RES_PATH), "Haru.model3.json")
+	model = factory.cubism_model(ProjectSettings.globalize_path(RES_PATH), "Natori.model3.json")
 	
-	model.apply_expression("F03")
+#	model.apply_expression("F03")
 #	model.update(1.0)
 	
 	# debug
@@ -45,98 +47,87 @@ func _ready() -> void:
 	textures = _load_textures(json["file_references"]["textures"], RES_PATH)
 	
 	drawables = model.drawables()
-	
-#	meshes.resize(drawables.size())
-	
+
+	# Mask index prepass
 	for drawable in drawables:
 		var d := CubismFactory.drawable(drawable)
+		if not d.masks.empty():
+			for i in d.masks:
+				if not i in mask_indices:
+					mask_indices.append(i)
+	
+	meshes.resize(drawables.size())
+	mesh_materials.resize(drawables.size())
+	
+	for d_idx in drawables.size():
+		if d_idx in mask_indices:
+			continue
 		
-		# debug
-#		if d.draw_order in draw_orders:
-#			print("draw order already exists %d" % d.draw_order)
-#		else:
-#			draw_orders.append(d.draw_order)
-#		if d.render_order in render_orders:
-#			print("render order already exists %d" % d.render_order)
-#		else:
-#			render_orders.append(d.render_order)
+		var d := CubismFactory.drawable(drawables[d_idx])
 		
-		var mesh := MeshInstance.new()
-		var array_mesh := ArrayMesh.new()
+		var mesh := _create_mesh(d)
 		
-		var array: Array = []
-		array.resize(Mesh.ARRAY_MAX)
-		
-		var vertices := PoolVector2Array()
-		var uvs := PoolVector2Array()
-		var indices := PoolIntArray()
-		
-		for pos in d.vertex_positions:
-			vertices.append(Vector2(pos.x, -pos.y))
-		for uv in d.vertex_uvs:
-			uvs.append(Vector2(uv.x, -uv.y))
-		for index in d.indices:
-			indices.append(index)
-		
-		array[Mesh.ARRAY_VERTEX] = vertices
-		array[Mesh.ARRAY_TEX_UV] = uvs
-		array[Mesh.ARRAY_INDEX] = indices
-		
-		array_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, array)
-		mesh.mesh = array_mesh
-
 		var mat := SpatialMaterial.new()
 		mat.albedo_texture = textures[d.texture_index]
 		mat.flags_transparent = true
+		mat.vertex_color_is_srgb = true
+		mat.params_blend_mode = SpatialMaterial.BLEND_MODE_MIX
+		mat.albedo_color.a = d.opacity
+		if d.render_order >= Material.RENDER_PRIORITY_MAX:
+			printerr("render order %d is greater than max %d" % [d.render_order, Material.RENDER_PRIORITY_MAX])
+			d.render_order = Material.RENDER_PRIORITY_MAX
 		mat.render_priority = d.render_order
-		if d.masks.empty():
-			mat.params_blend_mode = SpatialMaterial.BLEND_MODE_MIX
-		else:
-			if "IS_INVERTED_MASK" in d.constant_flags_string:
-				# Looks like this requires a stencil buffer that doesn't exist in Godot
-				pass
-			else:
-				# Looks like this requires a stencil buffer that doesn't exist in Godot
-				pass
-
 		if "BLEND_ADDITIVE" in d.constant_flags_string:
 			mat.params_blend_mode = SpatialMaterial.BLEND_MODE_ADD
 		elif "BLEND_MULTIPLICATIVE" in d.constant_flags_string:
 			mat.params_blend_mode = SpatialMaterial.BLEND_MODE_MUL
 		else:
 			mat.params_blend_mode = SpatialMaterial.BLEND_MODE_MIX
-		mat.vertex_color_is_srgb = true
-		mat.albedo_color.a = d.opacity
-		if mat.albedo_color.a < 0.1:
-			mesh.name = "%s_%d" % [mesh.name, d.opacity]
-			pass
-#		print(d.opacity)
-
+		
 		mesh.set_surface_material(0, mat)
 		
-		# debug
-		for mask in d.masks:
-			if masks.has(mask):
-				print("%s mask exists" % mask)
-			else:
-				masks.append(mask)
+		meshes[d_idx] = mesh
+		mesh_materials[d_idx] = mat
 		
-		var v_material := ShaderMaterial.new()
-		var f_material := ShaderMaterial.new()
-		
-#		mesh.texture = textures[d.texture_index]
-		meshes.append(mesh)
-#		meshes[d.render_order] = mesh
 		root.add_child(mesh)
-	
-#	print(meshes)
-#	print(masks)
+		
+		if not d.masks.empty():
+			for mask_idx in d.masks:
+				if mask_idx == -1:
+					continue
+				if meshes[mask_idx] != null:
+					print("mask already processed %d" % mask_idx)
+					continue
+				
+				var mask := CubismFactory.drawable(drawables[mask_idx])
+				var mask_mesh := _create_mesh(mask)
+				
+				print(d.constant_flags_string)
+				print(d.dynamic_flags_string)
+				
+				var mask_mat := ShaderMaterial.new()
+				if "IS_INVERTED_MASK" in d.constant_flags_string:
+					mask_mat.shader = load("res://shaders/inverted_mask.shader")
+				else:
+					mask_mat.shader = load("res://shaders/mask.shader")
+				
+				mask_mat.render_priority = d.render_order
+				mask_mat.set_shader_param("u_tex_0", textures[d.texture_index])
+				mask_mat.set_shader_param("u_tex_1", textures[mask.texture_index])
+				
+				mask_mesh.set_surface_material(0, mask_mat)
+				
+				meshes[mask_idx] = mask_mesh
+				mesh_materials[mask_idx] = mat
+				
+				root.add_child(mask_mesh)
+				print(mask_mesh)
 
 func _process(delta: float) -> void:
 	model.update(delta)
 	drawables = model.drawables()
-	_draw_mesh()
-	model.apply_expression("F01")
+#	_draw_mesh()
+#	model.apply_expression("F01")
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
@@ -166,6 +157,36 @@ func _load_textures(paths: Array, res_path: String) -> Array:
 		textures.append(image_texture)
 	
 	return textures
+
+func _create_mesh(d: CubismFactory.Drawable) -> MeshInstance:
+	var mesh := MeshInstance.new()
+	var array_mesh := ArrayMesh.new()
+	
+	var array: Array = []
+	array.resize(Mesh.ARRAY_MAX)
+	
+	var vertices: PoolVector2Array = []
+	var uvs: PoolVector2Array = []
+	var indices: PoolIntArray = []
+	
+	for pos in d.vertex_positions:
+		vertices.append(Vector2(pos.x, -pos.y))
+	for uv in d.vertex_uvs:
+		uvs.append(Vector2(uv.x, -uv.y))
+	for index in d.indices:
+		indices.append(index)
+	
+	array[Mesh.ARRAY_VERTEX] = vertices
+	array[Mesh.ARRAY_TEX_UV] = uvs
+	array[Mesh.ARRAY_INDEX] = indices
+	
+	if indices.size() == 0:
+		printerr("indices are empty for drawable %d" % d.index)
+	
+	array_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, array)
+	mesh.mesh = array_mesh
+	
+	return mesh
 
 func _draw_mesh() -> void:
 	var visited_idx: Array = []
@@ -199,7 +220,8 @@ func _draw_mesh() -> void:
 #			var array_mesh := ArrayMesh.new()
 			var array_mesh: ArrayMesh = meshes[mask_idx].mesh
 			var array: Array = meshes[mask_idx].mesh.surface_get_arrays(0)
-			var mat: SpatialMaterial = array_mesh.surface_get_material(0)
+#			var mat: SpatialMaterial = array_mesh.surface_get_material(0)
+			var mat: Material = mesh_materials[mask_idx]
 			array_mesh.clear_surfaces()
 			
 			var vertices = array[Mesh.ARRAY_VERTEX]
@@ -254,7 +276,8 @@ func _draw_mesh() -> void:
 #		var array_mesh := ArrayMesh.new()
 		var array_mesh: ArrayMesh = m.mesh
 		var array: Array = m.mesh.surface_get_arrays(0)
-		var mat: SpatialMaterial = array_mesh.surface_get_material(0)
+#		var mat: SpatialMaterial = array_mesh.surface_get_material(0)
+		var mat: SpatialMaterial = mesh_materials[drawable_idx]
 		array_mesh.clear_surfaces()
 		
 		var vertices = array[Mesh.ARRAY_VERTEX]
